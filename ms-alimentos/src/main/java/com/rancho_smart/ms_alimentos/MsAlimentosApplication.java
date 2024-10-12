79d8800c26c2f8c048fd06753541dc62d01a4be0
el-sebas-galeano <jsebastian-galeano@javeriana.edu.co>
2024-10-12 03:27:42 -0500

    :sparkles: feat: Creación de la Aplicación SpringBoot.

---
+++ ms-alimentos/src/main/java/com/rancho_smart/ms_alimentos/MsAlimentosApplication.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.ms_alimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAlimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAlimentosApplication.class, args);
+	}
+
+}
---
+++ ms-alimentos/src/test/java/com/rancho_smart/ms_alimentos/MsAlimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.ms_alimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAlimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
